8472433b8b7afa235149b20719736d8da4df2f69
emmanuel <emmanuel.tejeda.snchez@gmail.com>
2022-03-07 22:24:05 -0600

    auth

--- src/app/models.ts
+++ src/app/models.ts
@@ -1,4 +1,13 @@
 
+export interface Cliente {
+    uid: string;
+    email: string;
+    celular: string;
+    foto: string;
+    nombre: string;
+    referencia: string;
+    ubicacion: any;
+ }
 
 export interface Producto {
     nombre: string;
---
+++ src/app/pages/perfil/perfil.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { Cliente } from 'src/app/models';
+import { FirebaseauthService } from '../../servicios/firebaseauth.service';
+import { FirestorageService } from '../../servicios/firestorage.service';
+import { FirestoreService } from '../../servicios/firestore.service';
+
+@Component({
+  selector: 'app-perfil',
+  templateUrl: './perfil.component.html',
+  styleUrls: ['./perfil.component.scss'],
+})
+export class PerfilComponent implements OnInit {
+
+  cliente: Cliente = {
+    uid: '',
+    email: '',
+    celular: '',
+    foto: '',
+    referencia: '',
+    nombre: '',
+    ubicacion: null,
+  };
+
+  newFile: any;
+
+  constructor(public menu: MenuController,
+              public firebaseauthService: FirebaseauthService,
+              public firestorageService: FirestorageService,
+              public firestoreService: FirestoreService) { }
+
+   async ngOnInit() {
+
+    const uid = await this.firebaseauthService.getUid();
+    console.log(uid);
+
+   }
+
+  openMenu(){
+    console.log('open menu');
+    this.menu.toggle('principal');
+  }
+
+
+  async newImageUpload(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      this.newFile = event.target.files[0];
+     const reader = new FileReader();
+     reader.onload = ((image) => {
+         this.cliente.foto = image.target.result as string;
+     });
+     reader.readAsDataURL(event.target.files[0]);
+   }
+ }
+
+ async registrarse(){
+    const credenciales = {
+      email: this.cliente.email,
+      password: this.cliente.celular
+    };
+   const res = await this.firebaseauthService.registrar(credenciales.email, credenciales.password).catch( err =>{
+     console.log('error->', err);
+    });
+   console.log(res);
+   const uid = await this.firebaseauthService.getUid();
+   this.cliente.uid = uid;
+   this.guardarUser();
+   console.log(uid);
+ }
+
+ async guardarUser() {
+  const path = 'Clientes';
+  const name = this.cliente.nombre;
+  if(this.newFile !== undefined){
+  const res = await this.firestorageService.uploadImage(this.newFile, path, name);
+  this.cliente.foto = res;
+}
+  // eslint-disable-next-line @typescript-eslint/no-shadow
+  this.firestoreService.createDoc(this.cliente, path, this.cliente.uid).then( res => {
+      console.log('guardado con exito');
+  }).catch(   error => {
+  });
+}
+
+ async salir(){
+  //const uid = await this.firebaseauthService.getUid();
+  //console.log(uid);
+  this.firebaseauthService.logout();
+ }
+
+}
